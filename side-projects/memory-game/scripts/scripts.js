5b7778b4c6f4ddd5de1e80312553cdc480869566
richard-FL <richard@furrylogic.net>
2015-11-06 12:22:35 -0500

    Memory Game
    Creating memory game.

---
+++ side-projects/memory-game/scripts/scripts.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Richard Hall on 11/6/2015.
+ */
+//$(document).load(function () {
+//
+
+    $("#card-1").flip({
+        axis: "x", // y or x
+        reverse: false, // true and false
+        speed: 1000
+    });
+    $("#card-2").flip({
+        axis: "x", // y or x
+        reverse: false, // true and false
+        speed: 1000
+    });
+    $("#card-3").flip({
+        axis: "x", // y or x
+        reverse: false, // true and false
+        speed: 1000
+    });
+    $("#card-4").flip({
+        axis: "x", // y or x
+        reverse: false, // true and false
+        speed: 1000
+    });
+    $("#card-5").flip({
+        axis: "x", // y or x
+        reverse: false, // true and false
+        speed: 1000
+    });
+
+//});
