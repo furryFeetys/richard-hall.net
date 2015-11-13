c6c061eb5da9e744f655da4b9a1c3ddf4b90ee89
furryFeetys <nick@furryLogic.net>
2015-11-13 09:19:17 -0500

    Made the memory game work

--- side-projects/memory-game/scripts/scripts.js
+++ side-projects/memory-game/scripts/scripts.js
@@ -6,30 +6,51 @@
 // variable that we are using to keep track of how many cards are clicked.
 var count = 0;
 var startClass = '';
-function checkClass(){
+function checkClass() {
     //alert ("first step");
-
     if ( $(".card").hasClass("clicked")){
         count++;
     }
 }
+
+/*
+
+/////////////// RICHARD THESE NOTES ARE FOR YOU!!! //////////
+// Nigga is "div" an ID? wtf?
+//var classList = document.getElementById('div').className.split(/\s+/);
 var classList = document.getElementById('div').className.split(/\s+/);
 for (var i = 0; i < classList.length; i++) {
     if (classList[i] === 'heart') {
         alert("I think this works?");
     }
 }
+// So I thought to do classList = $(this).getElement...
+// That didn't quite work. Here's why.
+// http://stackoverflow.com/questions/4069982/document-getelementbyid-vs-jquery
+// $(this)[0]... works, but because we're looking at .card).click, it gives me the class of the card div, not the span.
+// So I added classes in the card div that correlate to the icons. We're checking against those classes now.
+//*/
 
 
 
 // when you click a card, this finds the "front card class" and triggers toggleClass to mark them as "clicked" then fades the front card covering the back card
 // finally, it triggers checkClass - which counts how many cards are "clicked"
 $ (".card").click(function() {
+
+    // Add 'clicked' class and fade out the front of the card.
     $(this).toggleClass("clicked").find(".front").fadeToggle("fast", "linear");
+
+
     //alert ("before function");
     checkClass();
+    var classList = $(this)[0].className.split(/\s+/);
+    //first-heart
+    //var classList = document.getElementById('first-heart').className.split(/\s+/);
+    //alert( classList[1] );
     // if startClass empty, assign the class icon of what we clicked into start class.
-    startClass = ''; // Set to whatever you click first.
+    if( '' == startClass ) {
+        startClass = classList[1];
+    }
 
     // if statClass not empty, compare the icon class of what we clicked to start class.
     // If they match, huzzah. GOt one right on memory game.
@@ -37,26 +58,30 @@ $ (".card").click(function() {
 
 
     // trying to establish a way to track things.
-    if ( $(".back").hasClass("heart") ){
-
-
-    }
+    //if ( $(".back").hasClass("heart") ){ }
 
     //alert (count);
 
     // when 2 objects have been clicked, this triggers a delayed toggleClass on the clicked objects, then toggles the fade so the cards revert back to default.
     if( count >= 2){
 
-        var divs = $(".clicked").find(".back");
-        console.log($(divs).get(0));
-        console.log($(divs).get(1));
-        alert(divs.length);
+        //var divs = $(".clicked").find(".back");
+        //console.log($(divs).get(0));
+        //console.log($(divs).get(1));
+        //alert(divs.length);
         //alert( divs.get(0) );
-        if (divs.get(0) == divs.get(1)) alert("Same");
+        //if (divs.get(0) == divs.get(1)) alert("Same");
         //if ($(".clicked").get(0) == $(".clicked").get(1) )alert("YEAH!");
         //$('.clicked *').attr('disabled','disabled').off('click');
 
-        $(".clicked").toggleClass("clicked").find(".front").delay( 2000).fadeToggle("fast", "linear");
+        if( startClass == classList[1] ) {
+            alert('You found a match!');
+            $(".clicked").toggleClass("clicked");
+        } else {
+            $(".clicked").toggleClass("clicked").find(".front").delay( 2000).fadeToggle("fast", "linear");
+        }
+        // Have to reset both of these for the next set.
+        startClass = '';
         count = 0;
     }
 });
